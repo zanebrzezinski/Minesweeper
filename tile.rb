cc7077b32d6912fe0e84c33cf0252ff0e72be1cd
App Academy Student <appacademy@Apps-Mac-mini-35.local>
2015-12-04 11:48:54 -0500

    Add Tile#in_bounds and DELTAS class constant.

--- tile.rb
+++ tile.rb
@@ -2,17 +2,40 @@ require 'byebug'
 
 class Tile
 
-  attr_accessor :hidden, :flagged
+  DELTAS = [
+    [ 0, 1],
+    [ 1, 1],
+    [ 1, 0],
+    [ 1,-1],
+    [ 0,-1],
+    [-1,-1],
+    [-1, 0],
+    [-1, 1]
+  ]
 
-  def initialize(bomb = false, position)
+
+  attr_accessor :hidden, :flagged
+  attr_reader :board_size, :position
+  
+  def initialize(bomb = false, position, board_size)
     @bomb = bomb
     @hidden = true
     @flagged = false
     @position = position
+    @board_size = board_size
   end
 
   def neighbors
-    
+
+    neighbors = []
+
+
+  end
+
+  def in_bounds?(position)
+
+    position.all? { |el| el.between?(0, board_size - 1)}
+
   end
 
   def reveal!
