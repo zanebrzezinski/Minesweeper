9f086c413e50021a44fb08412cc1189b66baaa0d
App Academy Student <appacademy@Apps-Mac-mini-35.local>
2015-12-04 11:15:12 -0500

    Add Tile#initialize and instance variables.

--- tile.rb
+++ tile.rb
@@ -2,7 +2,18 @@ require 'byebug'
 
 class Tile
 
-  
+  attr_accessor :hidden?, :flag?
+
+  def initialize(bomb = false)
+    @bomb? = bomb
+    @hidden? = true
+    @flagged? = false
+  end
+
+  def reveal
+    
+
+  end
 
 
 
