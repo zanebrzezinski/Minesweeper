83bcc1341fe67393677a7e473a31e2f54e6eb2d1
App Academy Student <appacademy@Apps-Mac-mini-35.local>
2015-12-04 11:37:38 -0500

    Add Tile readers for hidden, flagged, and bomb.

--- tile.rb
+++ tile.rb
@@ -2,16 +2,33 @@ require 'byebug'
 
 class Tile
 
-  attr_accessor :hidden, :flag
+  attr_accessor :hidden, :flagged
 
-  def initialize(bomb = false)
+  def initialize(bomb = false, position)
     @bomb = bomb
     @hidden = true
     @flagged = false
+    @position = position
+  end
+
+  def neighbors
+    
   end
 
   def reveal!
     self.hidden = false
+  end
+
+  def hidden?
+    @hidden
+  end
+
+  def flagged?
+    @flagged
+  end
+
+  def bomb?
+    @bomb
   end
 
 
