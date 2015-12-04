39e3b5cc0aae331f58adf4ec8170796d9cf3230c
App Academy Student <appacademy@Apps-Mac-mini-35.local>
2015-12-04 12:27:43 -0500

    Create Board class, Board#initialize, and #make_gird and #populate.

---
+++ board.rb
@@ -0,0 +1,31 @@
+require_relative 'tiles'
+class Board
+
+  attr_reader :size, :bomb_num
+
+  def initialize(size, bomb_num = nil)
+    @size = size
+    @grid = make_grid
+    @bomb_num ||= size
+  end
+
+  def make_grid
+
+    grid = Array.new(size) {Array.new(size)}
+    populate(grid)
+
+  end
+
+  def populate(grid)
+
+    bomb_positions = []
+    until bomb_positions.size == bomb_num
+      new_position = [rand(size), rand(size)]
+      unless bomb_positions.include?(new_position)
+        bomb_positions << new_position
+      end
+    end
+
+  end
+
+end
