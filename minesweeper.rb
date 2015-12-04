819463d33bfe46dca331f89c1337d50c7fbc5ec0
RAFAEL and ZANE <appacademy@Apps-Mac-mini-35.local>
2015-12-04 15:03:20 -0500

    Create Minesweep#initialize and get values from user.

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,4 +1,21 @@
 require_relative 'board'
 
 class MineSweeper
+
+  def initialize
+    @board = Board.new(*get_values)
+  end
+
+  def get_values
+    puts "Enter board size:"
+    size = gets.to_i
+    puts "Enter bomb number:"
+    bombs = gets.to_i
+    [size, bombs]
+  end
+
+  def play
+    
+  end
+
 end
